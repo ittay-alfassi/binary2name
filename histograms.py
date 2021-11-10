6b8deda31ae071d7fc0d1e962f81a9994dab0cf1
Itamar Juwiler <itamarj@campus.technion.ac.il>
2021-11-10 12:09:52 +0200

    Updated histogram script
    added more stats and amplified the script

--- histograms.py
+++ histograms.py
@@ -2,9 +2,14 @@ import json
 from typing import Dict, List
 import argparse
 import os
+from metadata import *
 
 BBL_HISTOGRAM = {}
 CONSTRAINT_HISTOGRAM = {}
+TOTAL_STATS_HISTOGRAM = []
+TOTAL_NODE_NUMBER = 0
+TOTAL_CONSTRAINT_NUM = 0
+TOTAL_CONSTRAINT_LEN = 0
 
 
 def add_to_hist(histogram: Dict[str, int], entry: str):
@@ -28,13 +33,11 @@ def register_tokens(graph_json: dict, file_name: str):
     for node in nodes:
         if not is_entry_junk(node['instructions']):
             add_to_hist(BBL_HISTOGRAM, node['instructions'])
-        if 'constraints' in node:
-            for constraint in node['constraints']:
-                filtered_cons = filter_constraint(constraint)
-                for con in filtered_cons:
-                    add_to_hist(CONSTRAINT_HISTOGRAM, con)
-                    if len(con) > 1000:
-                        print('look out, really big constraint in:', file_name)
+
+        for constraint in node['constraints']:
+            filtered_cons = filter_constraint(constraint)
+            for con in filtered_cons:
+                add_to_hist(CONSTRAINT_HISTOGRAM, con)
 
 
 def get_all_filenames(base_path: str):
@@ -48,7 +51,18 @@ def get_all_filenames(base_path: str):
     return res
 
 
-if __name__ == '__main__':
+def calc_total_stats(file_dict: dict, filename: str) -> None:
+    global TOTAL_NODE_NUMBER
+    global TOTAL_CONSTRAINT_NUM
+    global TOTAL_CONSTRAINT_LEN
+    file_stats_dict = calc_constraint_stats(file_dict, filename)
+    TOTAL_STATS_HISTOGRAM.append(file_stats_dict)
+    TOTAL_NODE_NUMBER += file_stats_dict['total_node_num']
+    TOTAL_CONSTRAINT_NUM += file_stats_dict['constraint_total_count']
+    TOTAL_CONSTRAINT_LEN += file_stats_dict['constraint_len_total']
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--base_dir', type=str, required=True)
     parser.add_argument('--hist_dir', type=str, required=True)
@@ -60,15 +74,26 @@ if __name__ == '__main__':
         with open(filename) as f:
             file_dict = json.load(f)
             register_tokens(file_dict, filename)
+            calc_total_stats(file_dict['GNN_DATA'], filename)
 
     real_hist_dir = 'preprocessed_histograms/' + args.hist_dir
     if not os.path.exists(real_hist_dir):
         os.mkdir(real_hist_dir)
+
     with open(os.path.join(real_hist_dir, 'bbl_hist.json'), 'w') as f:
         json.dump(BBL_HISTOGRAM, f, indent=4, sort_keys=True)
 
     with open(os.path.join(real_hist_dir, 'constraint_hist.json'), 'w') as f:
         json.dump(CONSTRAINT_HISTOGRAM, f, indent=4, sort_keys=True)
+
+    with open(os.path.join(real_hist_dir, 'dataset_hist.json'), 'w') as f:
+        data_dict = {'total_node_number': TOTAL_NODE_NUMBER, 'total_constraint_number': TOTAL_CONSTRAINT_NUM,
+                     'total_constraint_len': TOTAL_CONSTRAINT_LEN, 'constraint_num_per_node': TOTAL_CONSTRAINT_NUM / TOTAL_NODE_NUMBER,
+                     'constraint_len_per_node': TOTAL_CONSTRAINT_LEN / TOTAL_NODE_NUMBER}
+        json.dump(data_dict, f, indent=4, sort_keys=True)
+
+    with open(os.path.join(real_hist_dir, 'per_file_hist.json'), 'w') as f:
+        json.dump(TOTAL_STATS_HISTOGRAM, f, indent=4, sort_keys=True)
 '''  
 TODO: need to separate constraints. the way to parse it is <cons1> <cons2>...
 need to be careful of < and | switching places, for example |<BOOL...
@@ -77,3 +102,6 @@ also remove spaces, its inconsistent
 lastly check why some constraints contain the token <...>
 its uninformative and harmful
 '''
+
+if __name__ == '__main__':
+    main()
--- metadata.py
+++ metadata.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 
 def get_instruction_count(block_dict: dict, delim="    ") -> int:
@@ -10,12 +10,12 @@ def get_constraint_count(block_dict: dict, delim="    ") -> int:
     assert "constraints" in block_dict
     constraints = block_dict["constraints"]
 
-    if constraints == []:
+    if not constraints:
         return 0
 
     if type(constraints[0]) == str:  # Support old version, in which every path is represented at as a string.
         constraints = [list(filter(None, con.split(delim))) for con in constraints]
-    
+
     lengths = [len(con_list) for con_list in constraints]
     return sum(lengths)
 
@@ -24,12 +24,24 @@ def get_constraint_len(block_dict: dict, delim="   ") -> int:
     assert "constraints" in block_dict
     constraints = block_dict["constraints"]
 
-    if constraints == []:
+    if not constraints:
         return 0
 
     if type(constraints[0]) == str:  # Support old version, in which every path is represented at as a string.
         constraints = [list(filter(None, con.split(delim))) for con in constraints]
-    
+
     lengths = [[len(con) for con in con_list] for con_list in constraints]
-    lengths = [sum(con_list) for con_list in constraints]
+    lengths = [sum(length) for length in lengths]
     return sum(lengths)
+
+
+def calc_constraint_stats(file_dict: dict, filename: str) -> dict:
+    # calculating the amount of constraints in each node and doing an average
+    total_count = [get_constraint_count(node_dict) for node_dict in file_dict['nodes']]
+    node_count = len(file_dict['nodes'])
+    total_length = [get_constraint_len(node_dict) for node_dict in file_dict['nodes']]
+
+    return {'filename': filename, 'total_node_num': node_count, 'constraint_amount_distrib': total_count,
+            'constraint_total_count': sum(total_count), 'constraint_amount_avg': sum(total_count) / node_count,
+            'constraint_len_total': sum(total_length),
+            'constraint_len_distrib': total_length, 'constraint_len_avg': sum(total_length) / node_count}
